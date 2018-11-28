26f4e8f054e55b08575b92a4236d7858c3d0242e
bialek <bialekrafal94@gmail.com>
2018-11-28 15:01:56 +0100

    react-router and styled component added, navigation in progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Home from './Home';
+import Skills from './Skills';
+import Portfolio from './Portfolio';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="container">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/skills">Skills</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">Portfolio</Link>
+            </li>
+          </ul>
+
+          <Route exact path="/" component={Home} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/portfolio" component={Portfolio} />
+        </div>
+      </Router>
     );
   }
 }
