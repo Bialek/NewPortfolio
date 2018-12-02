225b11221731f5e9a1c0e110a88e52413662d40b
bialek <bialekrafal94@gmail.com>
2018-12-02 20:06:17 +0100

    skills page in progress

--- src/Skills.js
+++ src/Skills.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Container, Header } from './Styled/App';
 
 const Skills = () => {
   return (
-    <div className="container">
-      <h1>Skills!!</h1>
-    </div>
+    <Container>
+      <Header>Skills</Header>
+    </Container>
   );
 }
 
