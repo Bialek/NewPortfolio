8884451adb0d3298e6c9c98208a824d24679d82c
bialek <bialekrafal94@gmail.com>
2018-12-10 18:56:36 +0100

    change static element to reuseble component

--- src/Skills.js
+++ src/Skills.js
@@ -1,83 +1,99 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, PrimaryHeader, SecondaryHeader, Paragraf, SkillWrapper, Certificate, CertificateImg } from './Styled/App';
 
-const Skills = () => {
-  return (
-    <Container>
-      <PrimaryHeader>Skills</PrimaryHeader>
-        <div>
-          <SkillWrapper>
-            <Paragraf>HTML</Paragraf>
-            <img src={process.env.PUBLIC_URL + "/images/icon1.png"} alt="icon" />
-          </SkillWrapper>
-          <SkillWrapper>
-            <Paragraf>CSS</Paragraf>
-            <img src={process.env.PUBLIC_URL + "/images/icon2.png"} alt="icon" />
-          </SkillWrapper>
-          <SkillWrapper>
-            <Paragraf>JavaScript</Paragraf>
-            <img src={process.env.PUBLIC_URL + "/images/icon3.png"} alt="icon" />
-          </SkillWrapper>
-          <SkillWrapper>
-            <Paragraf>SASS</Paragraf>
-            <img src={process.env.PUBLIC_URL + "/images/icon4.png"} alt="icon" />
-          </SkillWrapper>
-          <SkillWrapper>
-            <Paragraf>Bootstrap</Paragraf>
-            <img src={process.env.PUBLIC_URL + "/images/icon5.png"} alt="icon" />
-          </SkillWrapper>
-          <SkillWrapper>
-            <Paragraf>React</Paragraf>
-            <img src={process.env.PUBLIC_URL + "/images/icon6.png"} alt="icon" />
-          </SkillWrapper>
-          <SkillWrapper>
-            <Paragraf>Redux</Paragraf>
-            <img src={process.env.PUBLIC_URL + "/images/icon7.png"} alt="icon" />
-          </SkillWrapper>
-          <SkillWrapper>
-            <Paragraf>jQuery</Paragraf>
-            <img src={process.env.PUBLIC_URL + "/images/icon8.png"} alt="icon" />
-          </SkillWrapper>
-          <SkillWrapper>
-            <Paragraf>Node</Paragraf>
-            <img src={process.env.PUBLIC_URL + "/images/icon9.png"} alt="icon" />
-          </SkillWrapper>
-          <SkillWrapper>
-            <Paragraf>NPM</Paragraf>
-            <img src={process.env.PUBLIC_URL + "/images/icon10.png"} alt="icon" />
-          </SkillWrapper>
-          <SkillWrapper>
-            <Paragraf>Git</Paragraf>
-            <img src={process.env.PUBLIC_URL + "/images/icon11.png"} alt="icon" />
-          </SkillWrapper>
-          <SkillWrapper>
-            <Paragraf>Unit tests</Paragraf>
-            <img src={process.env.PUBLIC_URL + "/images/icon12.png"} alt="icon" />
-          </SkillWrapper>
-        </div>
-
-      <SecondaryHeader>Certificates</SecondaryHeader>
-        <div>
-          <Certificate href={process.env.PUBLIC_URL + "/images/certificate/kodilla.jpg"} rel="noopener noreferrer" target="_blank">
-            Kodilla Bootcamp
-            <CertificateImg src={process.env.PUBLIC_URL + "/images/certificate/kodilla-m.jpg"} alt="Certificate" />
-          </Certificate>
-          <Certificate href={process.env.PUBLIC_URL + "/images/certificate/html.jpg"} rel="noopener noreferrer" target="_blank">
-            SoloLearn HTML
-            <CertificateImg src={process.env.PUBLIC_URL + "/images/certificate/html-m.jpg"} alt="Certificate" />
-          </Certificate>
-          <Certificate href={process.env.PUBLIC_URL + "/images/certificate/css.jpg"} rel="noopener noreferrer" target="_blank">
-            SoloLearn CSS
-            <CertificateImg src={process.env.PUBLIC_URL + "/images/certificate/css-m.jpg"} alt="Certificate" />
-          </Certificate>
-          <Certificate href={process.env.PUBLIC_URL + "/images/certificate/js.jpg"} rel="noopener noreferrer" target="_blank">
-            SoloLearn JavaScript
-            <CertificateImg src={process.env.PUBLIC_URL + "/images/certificate/js-m.jpg"} alt="Certificate" />
-          </Certificate>
-        </div>
-
-    </Container>
-  );
+class Skills extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      skillIcons: [
+        [
+          '/images/skills/icon1.png',
+          'HTML'
+        ],
+        [
+          '/images/skills/icon2.png',
+          'CSS'
+        ],
+        [
+          '/images/skills/icon3.png',
+          'JavaScript'
+        ],
+        [
+          '/images/skills/icon4.png',
+          'SASS'
+        ],
+        [
+          '/images/skills/icon5.png',
+          'Bootstrap'
+        ],
+        [
+          '/images/skills/icon6.png',
+          'React'
+        ],
+        [
+          '/images/skills/icon7.png',
+          'Redux'
+        ],
+        [
+          '/images/skills/icon8.png',
+          'jQuery'
+        ],
+        [
+          '/images/skills/icon9.png',
+          'Node'
+        ],
+        [
+          '/images/skills/icon10.png',
+          'NPM'
+        ],
+        [
+          '/images/skills/icon11.png',
+          'Git'
+        ],
+        [
+          '/images/skills/icon12.png',
+          'Unit tests'
+        ],
+        
+      ]
+    }
+  }
+  render() {
+    return (
+      <Container>
+        <PrimaryHeader>Skills</PrimaryHeader>
+          <div>
+            {this.state.skillIcons.map((skill, k) => (
+              <SkillWrapper key={k}>
+                <Paragraf>{skill[1]}</Paragraf>
+                <img src={process.env.PUBLIC_URL + skill[0] } alt='icon' />
+              </SkillWrapper>
+            ))}
+          </div>
+  
+        <SecondaryHeader>Certificates</SecondaryHeader>
+          <div>
+            <Certificate href={process.env.PUBLIC_URL + '/images/certificate/kodilla.jpg'} rel='noopener noreferrer' target='_blank'>
+              Kodilla Bootcamp
+              <CertificateImg src={process.env.PUBLIC_URL + '/images/certificate/kodilla-m.jpg'} alt='Certificate' />
+            </Certificate>
+            <Certificate href={process.env.PUBLIC_URL + '/images/certificate/html.jpg'} rel='noopener noreferrer' target='_blank'>
+              SoloLearn HTML
+              <CertificateImg src={process.env.PUBLIC_URL + '/images/certificate/html-m.jpg'} alt='Certificate' />
+            </Certificate>
+            <Certificate href={process.env.PUBLIC_URL + '/images/certificate/css.jpg'} rel='noopener noreferrer' target='_blank'>
+              SoloLearn CSS
+              <CertificateImg src={process.env.PUBLIC_URL + '/images/certificate/css-m.jpg'} alt='Certificate' />
+            </Certificate>
+            <Certificate href={process.env.PUBLIC_URL + '/images/certificate/js.jpg'} rel='noopener noreferrer' target='_blank'>
+              SoloLearn JavaScript
+              <CertificateImg src={process.env.PUBLIC_URL + '/images/certificate/js-m.jpg'} alt='Certificate' />
+            </Certificate>
+          </div>
+  
+      </Container>
+    );
+  }
 }
 
 export default Skills;
